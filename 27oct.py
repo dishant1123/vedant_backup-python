c7561dc726898dfbf012a17429490ec3b311cf3c
dishant1123 <dishantshah1417@gmail.com>
2025-10-27 13:54:33 +0530

    introduction

---
+++ 27oct.py
@@ -0,0 +1,48 @@
+"""
+python : 
+
+Guido van Rossum  ==>  developer  
+
+use : 
+
+1. data cleaning 
+2. AI , ML 
+3. GUI ==> graphical user interface  ==> game 
+4. data visualization / analysis 
+5. data wrapping  
+
+==================
+
+use  app in python  : 
+
+1. you tube  2. PUBG  3. Spotify 
+===================
+
+extension  : .py 
+
+===================
+
+library  ==> numpy , matplotlib , pandas etc  .. 
+
+===================
+run : 
+
+1. vs code  2. spider  3.jpyter notebook  4. google colab 
+
+"""
+
+
+'''
+print("my name is vedant")
+print('my name is vedant patel\n')
+print("ram")
+print("sita")
+print("ravan")
+print("laxman")
+'''
+# comment  in python  : 
+"""
+1. single  line  comment :   #  
+2. multi line  comment : ''' '''  or """ """
+
+"""
