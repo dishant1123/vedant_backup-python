9544d2a748e899c9c9836dd88c2645b1aec0a461
dishant1123 <dishantshah1417@gmail.com>
2025-10-28 13:37:37 +0530

    ....

---
+++ 28 oct.py
@@ -0,0 +1,89 @@
+# escape sequence  chaeacter : 
+"""
+1. \n : new line  
+2. \t : tab space
+3. \b : backspace
+4. end= : remove the  white  space
+"""
+# vedant patel
+"""
+print("vedant",end="\b\b")
+print("patel")
+"""
+
+# data type  : 
+"""
+1. int  : pos  or  neg store  ==> no limit 
+2. float  : pos  or  neg store  ==> no limit
+3. char / string  : a to z , name  , special character
+4.bool : true or false
+5/ complex number  : 2 part  : 1. real  2. imaginary
+"""
+"""
+a=100 
+print(a)
+print(type(a))
+
+b=123456234523452345634534534523452345345345
+print(b)
+print("b=",b)  # comma  create space in python
+print(type(b))
+
+c=12.45 
+print(c)
+print(type(c))
+
+d=1123456.234567823456789456789
+print(d)
+print(type(d))
+
+e=True
+print(e)
+print(type(e))
+
+f="1234@#$%^&"
+print(f)
+print(type(f))
+"""
+# complex number  : digit  i or  j  complex  
+
+"""g=12 +56j   # real part : 12  imaginary part : 56j 
+print(g)
+print(type(g))
+
+h=10 +45j 
+print(h+g)
+"""
+
+# user  input string :
+
+"""a=input("enter your  name  : ") 
+b=str(input("enter the  sur-name: "))
+print(a)
+print(b)
+print(a,b)
+print(a+" "+b)
+"""
+
+# user input  number : 
+"""
+a=int(input("enter the number 1 :"))
+b=int(input("enter the number 2 :"))
+print(a)
+print(b)
+print(a,b)
+print(a+b)
+"""
+a=float(input("enter the number 1 :"))
+b=float(input("enter the number 2 :"))
+print(a)
+print(b)
+
+# task  :1 
+"""
+ask user to enter two number (int type) and  concate  them. 
+input  a:10 
+input  b:20
+output : 1020
+
+"""
