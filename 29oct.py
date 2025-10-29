17cf0b6a8c71ae8166b92a6e6181de99226b4b35
dishant1123 <dishantshah1417@gmail.com>
2025-10-29 13:27:33 +0530

    ...

---
+++ 29oct.py
@@ -0,0 +1,25 @@
+# operator  : 
+"""
+1. airthematic  operator  : + - * / % // 
+2. comparsion  operator  : == != > < >= <=    ==> bool value  ==> true or false
+3. logic  operator  : and or not
+4. assignment  operator  : = += -= *= /= %= //=
+5. relationship  operator  : in  not in
+
+"""
+
+a=10
+b=100
+# print("sum is  :",a+b)
+# print("difference is :",a-b)
+# print("product is :",a*b)
+# print("divison is :",a/b) # may be float  value 
+# print("floor division is :",a//b)  # give  int  value only  
+# print("remainder is :",a%b)
+
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+# print(a==b)
+print(a!=b)
