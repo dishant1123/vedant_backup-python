bdc2f74d2beb3f507dd109d0b6efe86cb2f68eb4
dishant1123 <dishantshah1417@gmail.com>
2025-10-30 13:54:42 +0530

    ..

--- 30oct.py
+++ 30oct.py
@@ -80,3 +80,14 @@ ask user  to enter the  number and check it is divisible by 5 or 11 or  both .
 """
 ask user to enter the character and convert into upper case to  lower case and vice versa.
 """
+
+ch=input("enter the character : ")
+
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' :
+    print("vowel")
+elif ch >='a' and ch <='z' :
+    print("consonant")
+elif ch >='0' and ch <='9':
+    print("digit")
+else :
+    print("special character")
