5934a0e267c95a8a84d1e9b55410020e4932c072
dishant1123 <dishantshah1417@gmail.com>
2025-11-20 13:01:27 +0530

    ...

---
+++ 20nov.py
@@ -0,0 +1,30 @@
+# match  : 
+
+a=int(input("enter the  number  : "))
+b=int(input("enter the  number  : "))
+
+print("1. addition")
+print("2. subtraction")
+print("3. multiplication")
+print("4. division")
+print("5. modular")
+print("6. floor division")
+
+choice =int(input("enter the choice : "))
+
+match choice :
+    case 1 :
+        print("addition of  two number is :",a+b)
+    case 2 :
+        print("subtraction of  two number is :",a-b)
+    case 3 :
+        print("multiplication of  two number is :",a*b)
+    case 4 :
+        print("division of  two number is :",a/b)
+    case 5 :
+        print("modular of  two number is :",a%b)
+    case 6 :
+        print("floor division of  two number is :",a//b)
+    case _ :
+        print("invalid choice")
+        
